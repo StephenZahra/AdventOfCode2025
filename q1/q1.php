51a9961589d742e95cb24a50f02031e05fd320e3
StephenZahra <stephenzahra101@gmail.com>
2025-12-08 00:46:22 +0100

    Question 1 finished

---
+++ q1/q1.php
@@ -0,0 +1,39 @@
+<?php
+    $start = 50;
+    $zeroCount = 0;
+
+    foreach(file("q1Input.txt") as $line) {
+        $leftTurn = explode('L', $line);
+        $rightTurn = explode('R', $line);
+
+        // left turn has the direction and count of turns, right turn does not so we go left
+        if(count($leftTurn) > count($rightTurn)){
+            for($turns = (int)$leftTurn[1]; $turns > 0; $turns--){
+                $start--;
+
+                if($start == -1){
+                    $start = 99;
+                }
+
+                if($turns == 1 && $start == 0){
+                    $zeroCount++;
+                }
+            }
+        }
+        else{
+            for($turns = (int)$rightTurn[1]; $turns > 0; $turns--){
+                $start++;
+
+                if($start == 100){
+                    $start = 0;
+                }
+
+                if($turns == 1 && $start == 0){
+                    $zeroCount++;
+                }
+            }
+        }
+    }
+
+    echo("Zero Count: $zeroCount");
+?>
